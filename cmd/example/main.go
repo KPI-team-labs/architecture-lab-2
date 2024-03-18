fdae711f54cc2c1b3e9ecf8b5feff4e4d74d69df
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-03-18 16:48:33 +0200

    handler and updated main.go to use it

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,27 +1,64 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	lab2 "github.com/KPI-team-labs/architecture-lab-2"
+	"os"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inConsoleTag  = flag.String("e", "", "Tag to read the expression from console")
+	inputFileTag  = flag.String("f", "", "Tag to read the expression from the input file")
+	outputFileTag = flag.String("o", "", "Tag to put the converted expression into the output file")
 )
 
 func main() {
+	var err error
+	var handler lab2.ComputeHandler
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if err := checkTags(inConsoleTag, inputFileTag); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 
-	res, _ := lab2.PrefixToInfix("+ 2 2")
-	fmt.Println(res)
+	handler = lab2.ComputeHandler{
+		Input:  bytes.NewBufferString(*inConsoleTag),
+		Output: os.Stdout,
+	}
+
+	if *inConsoleTag == "" && *inputFileTag != "" {
+		inFile, err := os.Open(*inputFileTag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer inFile.Close()
+
+		handler.Input = inFile
+	}
+
+	if *outputFileTag != "" {
+		outfile, _ := os.OpenFile(*outputFileTag, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0777)
+		handler.Output = outfile
+		defer outfile.Close()
+	}
+
+	err = handler.Compute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Errors: %s", err)
+	}
+}
+
+func checkTags(e, f *string) error {
+	if *e == "" && *f == "" {
+		return errors.New("you must set or -e tag or -f tag")
+	}
+	if *e != "" && *f != "" {
+		return errors.New("you cannot use -e and -f tags at the same time")
+	}
+	return nil
 }
